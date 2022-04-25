c3b78bbd3a6fce417c81b61d85830ec31740011a
Your Name <you@example.com>
2022-04-25 10:14:58 +0800

    modified:   .gitignore 	modified:   _posts/test.mdx 	modified:   package-lock.json 	modified:   package.json 	modified:   src/lib/mdx.ts

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -23,6 +23,7 @@ import mdxPrism from "mdx-prism";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import markdown from "remark-parse";
+import remarkGfm from 'remark-gfm'
 
 const serializePost = async (
   content: string
@@ -33,6 +34,7 @@ const serializePost = async (
         markdown,
         remarkUnwrapImages,
         remarkCodeTitles,
+        [remarkGfm],
         [remarkMath],
         [remarkCapitalize],
         [
